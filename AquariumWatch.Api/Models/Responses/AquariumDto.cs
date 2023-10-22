9d5267427dbeb325f9f5fcb432b74ebff94d398c
Calum Grant <calum.grant@craveinteractive.com>
2023-10-22 23:59:52 +0200

    Added some required attributes.

--- AquariumWatch.Api/Models/Responses/AquariumDto.cs
+++ AquariumWatch.Api/Models/Responses/AquariumDto.cs
@@ -1,17 +1,23 @@
 ﻿using AquariumWatch.Data.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace AquariumWatch.Api.Models.Responses
 {
     public class AquariumDto
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = null!;
+        [Required]
         public double HighTemp { get; set; }
+        [Required]
         public double LowTemp { get; set; }
         public double Ph { get; set; }
         public double Ammonia { get; set; }
         public double Nitrite { get; set; }
         public double Nitrate { get; set; }
+        [Required]
         public AquariumType Type { get; set; }
     }
 }
