7c7fa5da1cef1c10c47e0a1dde3cc90892df5bb4
Calum Grant <calumgrant1@live.co.uk>
2024-01-07 17:42:41 +0100

    Add desc property to AquariumDto

--- AquariumWatch.Api/Models/Responses/AquariumDto.cs
+++ AquariumWatch.Api/Models/Responses/AquariumDto.cs
@@ -7,17 +7,25 @@ namespace AquariumWatch.Api.Models.Responses
     {
         [Required]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; } = null!;
+
+        [Required] 
+        public string Description { get; set; } = null!;
+
         [Required]
         public double HighTemp { get; set; }
+
         [Required]
         public double LowTemp { get; set; }
+
+        [Required]
+        public AquariumType Type { get; set; }
+
         public double Ph { get; set; }
         public double Ammonia { get; set; }
         public double Nitrite { get; set; }
         public double Nitrate { get; set; }
-        [Required]
-        public AquariumType Type { get; set; }
     }
 }
